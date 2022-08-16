2fd2b08675e62139767a5b312bb386b3125f302b
Andro Chen <androchentw@gmail.com>
2022-08-16 13:09:08 +0800

    docs: simplify comments

--- sample/pgfunction.py
+++ sample/pgfunction.py
@@ -22,23 +22,20 @@ def demo_enum():
     print(week.Monday.name)  # Monday
     print(week.Monday.value)  # 1
 
-    print(Color.RED.value)  # color1 獲得 (255, 0, 0)
-    print(Color.random_color())  # 隨機獲得顏色 RGB
+    print(Color.RED.value)  # (255, 0, 0)
+    print(Color.random_color())
 
 
-# 建立一個列舉型態的類別
-# @unique 能夠檢查列舉是否有重複的列舉值
-# 在參數中填上 Enum 宣告為列舉型態
+# @unique = check dupplicated enum
 @unique
 class Color(Enum):
-    # 定義一些基本顏色作為列舉名稱，而列舉值是其對應的RGB數值
     RED = (255, 0, 0)
     GREEN = (0, 255, 0)
     BLUE = (0, 0, 255)
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
 
-    # 定義靜態方法，可以由外部直接使用
+    # @staticmethod = public usable
     @staticmethod
     def random_color():
         r = randint(0, 255)
