601d9ea05f42214e784cb78d2c4c31251e3e5388
Andro Chen <androchentw@gmail.com>
2022-09-02 11:18:28 +0800

    refactor: rearrange test cases and report

--- sample/pgtest.py
+++ sample/pgtest.py
@@ -8,7 +8,7 @@ Reference
 Requirements
   $ pip3 install pytest
 
-Usage: python pgtest.py
+Usage: python3 pgtest.py
 """
 import argparse
 import subprocess
@@ -19,22 +19,7 @@ def main():
     args = parse_args()
     print(type(args.throw_test_fail))
     demo_unit_test(args.throw_test_fail)
-
-
-def demo_unit_test(throw_test_fail=False):
-    # $ allure serve reports/allure
-    result = subprocess.run(
-        f"python3 -m pytest test/ --capture=tee-sys --junitxml=reports/junit.xml \
-            --html=reports/report.html --self-contained-html \
-            --alluredir=reports/allure",
-        shell=True,
-        check=throw_test_fail,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        encoding="utf-8",
-    )
-    print(f"result.stdout\n{result.stdout}")
-    print(f"result.stderr\n{result.stderr}")
+    integrate_test_report()
 
 
 def parse_args():
@@ -48,6 +33,33 @@ def parse_args():
         help="Throw test fail exit status",
     )
     return parser.parse_args()
+
+
+def demo_unit_test(throw_test_fail=False):
+    # $ allure serve reports/allure
+    run_shell_command(
+        "python3 -m pytest test/ --capture=tee-sys --junitxml=reports/junit.xml \
+            --html=reports/report.html --self-contained-html \
+            --alluredir=reports/allure",
+        throw_test_fail,
+    )
+
+
+def integrate_test_report():
+    run_shell_command("genbadge tests --input-file reports/junit.xml")
+    run_shell_command("mv tests-badge.svg reports/")
+
+
+def run_shell_command(command="ls", check_fail=False):
+    result = subprocess.run(
+        command,
+        shell=True,
+        check=check_fail,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    print(f"run_shell_command: {command}  \n{result.stdout} \n {result.stderr}")
 
 
 if __name__ == "__main__":
